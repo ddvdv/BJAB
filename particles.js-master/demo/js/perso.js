f1cd99ac4e7b04e085d93bfa829dfb75cd41e49e
ddvdv <vdv.david@gmail.com>
2017-10-24 23:31:53 +0200

    avec anim

--- particles.js-master/demo/js/perso.js
+++ particles.js-master/demo/js/perso.js
@@ -12,11 +12,14 @@ $.fn.extend({
 $(document).ready(function(){
   let clickToUncover = 0;
   let hidden = true;
+  let password = "bjab to code";
+  let guess = '';
 
-  // Révélation du site
+
+  // Révélation du site par click
   $('canvas').click(function(){
     clickToUncover++;
-    if (clickToUncover >= 5 && hidden == true){
+    if (clickToUncover >= 15 && hidden == true){
       hidden = false;
       $('.own-content').css('z-index', 1);
       $('#logo').removeClass();
@@ -24,18 +27,47 @@ $(document).ready(function(){
     }
   })
 
+  // Révélation du site par konami
+  $('body').on('keypress', (e) => {
+    guess = guess + e.key;
+    console.log(e.key);
+    console.log(guess);
+    console.log(password);
+
+    if (guess == password){
+      hidden = false;
+      $('.own-content').css('z-index', 1);
+      $('#logo').removeClass();
+      $('#logo').animateCss('jackInTheBox');
+      $('body').off();
+    }
+  })
+
+
+
+
   // Effet sur le logo principal
   $('#logo').click(function(){
-  $('#logo').removeClass();
-  $('#logo').animateCss('jello');
+    $('#logo').removeClass();
+    $('#logo').animateCss('hinge');
   })
 
   // Affichage print-screen sur hover des options
   $('.option').mouseenter(function(){
     $(this).find('.scrn').removeClass('hidden');
-    $(this).find('.scrn').animateCss('bounceIn');
+    $(this).find('h3').animateCss('pulse');
   }).mouseleave(function(){
     $(this).find('.scrn').addClass('hidden');
+  })
+
+
+  $('.option').click(function(e){
+    e.preventDefault();
+    $(this).animateCss('rollOut');
+    setTimeout(()=>{
+      let link = $(this).find("a").attr("href");
+      window.open(link, '_blank');
+    }, 700);
   })
 
 
